dc0e6bc254c9b55a97cf340e58a9c5b28db0a511
ammar-64 <ammar.n.sammour@gmail.com>
2021-02-26 14:08:14 +0300

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+    attr_reader :name
+    attr_accessor :backed_projects
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+    def backed_projects
+        project_backers = ProjectBacker.all.select do |pb|
+          pb.backer == self
+        end
+        project_backers.map do |pb|
+          pb.project
+        end
+      end
+end
---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+    attr_reader :title
+    attr_accessor :backers
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+    def backers
+        project_backers = ProjectBacker.all.select do |pb| 
+          pb.project == self
+        end
+        project_backers.map do |pb|
+          pb.backer
+        end
+      end
+end
